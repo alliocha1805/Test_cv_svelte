0e57d64188b7cdcebe3810b842f5f7c28f507bc8
alliocha1805 <alexis.piloy@gmail.com>
2023-04-03 18:12:16 +0200

    1 file modified

--- src/routes/Experiences.svelte
+++ src/routes/Experiences.svelte
@@ -8,35 +8,51 @@
     <div class="timeline-block timeline-block-right">
         <div class="marker"></div>
         <div class="timeline-content">
-          <h3>First Year</h3>
-          <span>Some work experience</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+          <h3>Product Owner Data / API - Stelliant</h3>
+          <span>2022 - Présent</span>
+          <p>
+            Gestion du Backlog et des Users Stories en methode Agile SaFe<br>
+            Priorisation et accompagnement des expert assurance sur la mise en place technique de leurs besoins<br>
+            Conception technico-fonctionelle des EDI / API<br>
+            Ecriture des Contrats d'interface WS/API (Swagger / XSD)<br>
+            Conception de l'ordonnancement des echanges de data<br>
+            Recherche et Developpement sur les sujets d'IA (catégorisation / Prediction)
+            Creation d'outil d'automatisation en Python <br>
+          </p>
         </div>
     </div>
   
     <div class="timeline-block timeline-block-left">
         <div class="marker"></div>
         <div class="timeline-content">
-          <h3>Second Year</h3>
-          <span>Some work experience</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+          <h3>Product Owner Data / API (consultant) - Stelliant</h3>
+          <span>Sept 2019 - 2022</span>
+          <p>Product Owner de la solution d'EDI du groupe Stelliant (pour le compte de Themis Conseil)</p>
         </div>
     </div>
   
     <div class="timeline-block timeline-block-right">
         <div class="marker"></div>
         <div class="timeline-content">
-          <h3>Third Year</h3>
-          <span>Some work experience</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+          <h3>Chef de projet Digital / Product Owner Consultant - La Mutuelle Generale</h3>
+          <span>2018 - 2019</span>
+          <p>
+            Gestion de projet refonte Espace adhérent / Espace client Entreprise<br>
+            Travail sur les flux de données Mutuelle ⇔ Sécurité sociale <br>
+            Conseil technico-fonctionnel sur le projet (Site Web et PWA Mobile)<br>
+            Rédaction de spécification fonctionnelle<br>
+            Livraison TFS des release (Préproduction et Production)<br>
+            Recette Technico-fonctionnelle<br>
+            Creation d'outil d'automatisation en Python <br>
+          </p>
         </div>
     </div>
   
     <div class="timeline-block timeline-block-left">
         <div class="marker"></div>
         <div class="timeline-content">
-          <h3>Fourth Year</h3>
-          <span>Some work experience</span>
+          <h3>Consultant Product Owner Themis Conseil</h3>
+          <span>Mai 2018 - Decembre 2021</span>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
         </div>
     </div>
@@ -50,3 +66,10 @@
         </div>
     </div>
   </div>
+
+<style>
+span {
+  color:black;
+  font-weight: 500;
+}
+</style>
