6f0754cf127558bb7a0a2afb4d4ff0ade358581a
alliocha1805 <alexis.piloy@gmail.com>
2024-04-07 14:26:29 +0200

    1 file modified, 1 deleted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import avatar from '$lib/images/avatar-shadow.png';
-    import linkedinLogo from '$lib/images/linkedin.png';
+    import linkedin from '$lib/images/linkedin.png';
     import githubLogo from '$lib/images/github.png';
     import CvPDF from '$lib/pdf/cv_alexis_piloy_2023.pdf';
     import NavBar from './NavBar.svelte';
@@ -34,7 +34,14 @@
                 <p><span style="font-family: 'Roboto', sans-serif; font-size: 32px;font-weight: bold;">Alexis Piloy</span><br>
                 <span style="font-family: 'Roboto', sans-serif; font-size: 24px;font-weight: bold;">Product Owner</span></p>
                 <p style="font-family: 'Roboto', sans-serif; font-size: 18px; text-align: justify;">Je suis Alexis Piloy, un product owner spécialisé dans les sujets Data, API, EDI et la tech. Fort de plusieurs années d'expérience dans le domaine, j'ai développé une expertise pointue dans la gestion de projets liés à la collecte, l'analyse et l'exploitation des données. Passionné par les nouvelles technologies, je m'attache à rester constamment à l'affût des dernières tendances pour proposer des solutions innovantes et efficaces. Mes compétences en matière de communication, de négociation et de gestion d'équipe me permettent de travailler efficacement en collaboration avec différents acteurs pour garantir la réussite de chaque projet.</p>
-                <p><a href="https://www.linkedin.com/in/alexis-piloy/" target="_blank" style ="color: black; text-decoration: underline;" rel="external nofollow"><img src={linkedinLogo} alt="Linkedin /"></a>  <a href="https://github.com/alliocha1805" target="_blank" style ="color: black; text-decoration: underline;" rel="external nofollow"><img src={githubLogo} alt="Github"></a></p>
+                <p>
+                    <a href="https://www.linkedin.com/in/alexis-piloy/" target="_blank" style ="color: black; text-decoration: underline;" rel="external nofollow">
+                        <img src={linkedin} alt="Linkedin">
+                    </a>  
+                    <a href="https://github.com/alliocha1805" target="_blank" style ="color: black; text-decoration: underline;" rel="external nofollow">
+                        <img src={githubLogo} alt="Github">
+                    </a>
+                </p>
                 <a href={CvPDF} target="_blank" rel="external nofollow" class="bouton" id="download_cv_button" style="margin-bottom: 10px;">Télécharger mon CV</a>
                 <!--<a href="#Contact" class="bouton" style="margin-bottom: 10px;">Me contacter</a>-->
             </div>
